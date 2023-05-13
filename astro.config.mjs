77eb414795bc667840b8de17e0faa4624d3e5638
voskanvl <voskanvl@gmail.com>
2023-05-14 00:46:55 +0300

    style import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config"
 import yaml from "@rollup/plugin-yaml"
 import vue from "@astrojs/vue"
+import sassGlobImports from "vite-plugin-sass-glob-import"
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [vue()],
     vite: {
-        plugins: [yaml()],
+        plugins: [yaml(), sassGlobImports()],
     },
     build: {
         assets: "assets",
