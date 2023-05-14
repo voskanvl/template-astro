ba66eca2eb67ffd2cb3ca1e2768133f800bb2535
voskanvl <voskanvl@gmail.com>
2023-05-14 17:34:38 +0300

    try vite-plugin-vue-css-modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config"
 import yaml from "@rollup/plugin-yaml"
 import vue from "@astrojs/vue"
 import sassGlobImports from "vite-plugin-sass-glob-import"
+import { cssm, removeCssModulesChunk } from "vite-plugin-vue-css-modules"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue()],
+    integrations: [cssm(), vue(), removeCssModulesChunk()],
     vite: {
         plugins: [yaml(), sassGlobImports()],
     },
